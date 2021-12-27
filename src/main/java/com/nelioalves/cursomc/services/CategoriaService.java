3a67a8f5c5bbbe3c61eb86a9f89b8c5703ac48dc
Frederico Rodarte <fredericorodarte12@gmail.com>
2021-12-27 11:27:05 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -4,26 +4,26 @@ package com.nelioalves.cursomc.services;
 
 import java.util.Optional;
 
-import org.hibernate.ObjectNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-  	
+
 	public Categoria find(Integer id) { 
-		Optional<Categoria> obj = repo.findById(id);
+		  Optional<Categoria> obj = repo.findById(id); 
+		  return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		        "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null)); 
+		}  	
+	
 
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
-
-		}
-		} 	
+	} 	
 
